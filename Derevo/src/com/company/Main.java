733b558752a05c749f797500a4e62a7a7eb3ce70
Andrei19941967 <sytenkovat@yandex.ru>
2024-01-16 15:13:53 +0300

    derev

---
+++ Derevo/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MyTree m=new MyTree();
+     m.Append(5);
+     m.Append(3);
+     m.Append(9);
+     System.out.println(m.Contains(2));
+    }
+}
---
+++ Derevo/src/com/company/Node.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Node {//класс описывет узел дерева
+    public int Value;//значение узла
+    public Node Left;//ссылка на левую ветвь
+    public Node Right;//ссылка на правую ветвь
+    public Node(int Value,Node Left,Node Right)
+    {
+     this.Value=Value;
+     this.Left=Left;
+     this.Right=Right;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf((Value));//преврашение числа в строку
+    }
+}
